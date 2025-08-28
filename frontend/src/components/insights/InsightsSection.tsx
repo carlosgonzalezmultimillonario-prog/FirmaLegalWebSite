4315f07b6f6116f8da2f21c01ba96cc6be30f3e9
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-08-28 07:48:56 -0500

    Change Box to Grid InsightsSection

--- frontend/src/components/insights/InsightsSection.tsx
+++ frontend/src/components/insights/InsightsSection.tsx
@@ -4,6 +4,7 @@ import { Typography, Box } from "@mui/material";
 import InsightsCard from "./InsightsCard";
 import { InsightsSectionProps } from "@/types/insights.types";
 import { insightsMock } from "@/mocks/insights.mocks";
+import  Grid  from "@mui/material/Grid";
 
 
 const InsightsSection: FC<InsightsSectionProps> = ({
@@ -20,21 +21,21 @@ const InsightsSection: FC<InsightsSectionProps> = ({
   }
 
   return (
-    <Box sx={{ 
-        pt: 20, 
-        pb: 0, 
-        backgroundColor: 'primary.main',
-        width: '100vw',
-        position: 'relative',
-        left: '50%',
-        right: '50%',
-        marginLeft: '-50vw',
-        marginRight: '-50vw'
-      }}>
+    <Box sx={{
+      pt: 20,
+      pb: 0,
+      backgroundColor: 'primary.main',
+      width: '100vw',
+      position: 'relative',
+      left: '50%',
+      right: '50%',
+      marginLeft: '-50vw',
+      marginRight: '-50vw'
+    }}>
       <Box sx={{ maxWidth: '90%', mx: 'auto' }}>
         <Box sx={{ mb: 12, px: 6 }}>
-          <Typography variant="h1" component="h2" 
-            sx={{ 
+          <Typography variant="h1" component="h2"
+            sx={{
               color: 'white',
               fontSize: '3.5rem',
               fontWeight: 300,
@@ -42,8 +43,8 @@ const InsightsSection: FC<InsightsSectionProps> = ({
             }}>
             {title}
           </Typography>
-          <Typography 
-            sx={{ 
+          <Typography
+            sx={{
               color: '#F3F0E9',
               fontSize: '1.125rem',
               opacity: 0.85,
@@ -52,28 +53,30 @@ const InsightsSection: FC<InsightsSectionProps> = ({
             {subtitle}
           </Typography>
         </Box>
-    
-        <Box 
-          sx={{ 
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, 1fr)',
-            gap: 4,
+        
+        <Grid
+          container
+          spacing={4}
+          sx={{
             mb: 8,
             maxWidth: '90%',
-            mx: 'auto'
+            mx: 'auto',
+            backgroundColor: 'black'
           }}
         >
-          {items.map((insight) => (
-            <Box 
-              key={insight.id} 
-              sx={{ 
-                width: '100%'
-              }}
+          {/* {items.map((insight) => (
+            <Grid 
+              item
+              key={insight.id}
+              xs={12}
+              md={6}    
+              
             >
               <InsightsCard item={insight} />
-            </Box>
-          ))}
-        </Box>
+            </Grid>
+          ))} */}
+        </Grid>
+
       </Box>
     </Box>
   );
