c1f9001e4a169bbaf441b57eff4a69e80ef7b9b4
Carlos Elio <carloseliogonzalezapa1997@gmail.com>
2025-08-29 09:22:50 -0500

    Update Insights grid layout to match Figma model

--- frontend/src/components/insights/InsightsSection.tsx
+++ frontend/src/components/insights/InsightsSection.tsx
@@ -4,7 +4,7 @@ import { Typography, Box } from "@mui/material";
 import InsightsCard from "./InsightsCard";
 import { InsightsSectionProps } from "@/types/insights.types";
 import { insightsMock } from "@/mocks/insights.mocks";
-import  Grid  from "@mui/material/Grid";
+import { Grid } from "@mui/system";
 
 
 const InsightsSection: FC<InsightsSectionProps> = ({
@@ -21,62 +21,51 @@ const InsightsSection: FC<InsightsSectionProps> = ({
   }
 
   return (
-    <Box sx={{
-      pt: 20,
-      pb: 0,
-      backgroundColor: 'primary.main',
-      width: '100vw',
-      position: 'relative',
-      left: '50%',
-      right: '50%',
-      marginLeft: '-50vw',
-      marginRight: '-50vw'
-    }}>
+    <Box sx={{ 
+        pt: 20, 
+        pb: 0, 
+        backgroundColor: 'primary.main',
+        width: '100vw',
+        position: 'relative',
+        left: '50%',
+        right: '50%',
+        marginLeft: '-50vw',
+        marginRight: '-50vw'
+      }}>
+         {/* Contenedor central único */}
       <Box sx={{ maxWidth: '90%', mx: 'auto' }}>
-        <Box sx={{ mb: 12, px: 6 }}>
-          <Typography variant="h1" component="h2"
-            sx={{
+         {/* Título y subtítulo */}
+        <Box sx={{ mb: 12 }}>
+          <Typography variant="h1" component="h2" 
+            sx={{ 
               color: 'white',
-              fontSize: '3.5rem',
+              fontSize: '8rem',
               fontWeight: 300,
               mb: 2
             }}>
             {title}
           </Typography>
-          <Typography
-            sx={{
-              color: '#F3F0E9',
-              fontSize: '1.125rem',
+          <Typography 
+            sx={{ 
+              color: 'white',
+              fontSize: '1.5rem',
               opacity: 0.85,
               pl: 0.5
             }}>
             {subtitle}
           </Typography>
         </Box>
-        
-        <Grid
-          container
-          spacing={4}
-          sx={{
-            mb: 8,
-            maxWidth: '90%',
-            mx: 'auto',
-            backgroundColor: 'black'
-          }}
-        >
-          {/* {items.map((insight) => (
+    
+        <Grid container sx={{ mb: 8}}>
+          {items.map((insight) => (
             <Grid 
-              item
-              key={insight.id}
-              xs={12}
-              md={6}    
-              
+              key={insight.id} 
+              size={{ xs: 12, md: 6 }} 
             >
               <InsightsCard item={insight} />
             </Grid>
-          ))} */}
+          ))}
         </Grid>
-
       </Box>
     </Box>
   );
